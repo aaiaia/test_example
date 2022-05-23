557a0f3085bc35f4e3929d88cf6da4d35dde82ac
PleYa <aaiaia.gg@gmail.com>
2022-05-24 01:42:36 +0900

    fixed threading.Event about detailed logs (#3)
    * fixed logs detail, decrease wait timeout
    
    * added print log do thread event to set()

--- example_codes/python/thread/event/run_thread.py
+++ example_codes/python/thread/event/run_thread.py
@@ -10,45 +10,54 @@ def thread_test(number, float_time, thread_evt, timeout_100ms=10):
     print(str(_currtime) + ', thread_test, number is \'' + str(number) + '\', time: \'' + str(float_time) + '\' [start]')
     for _i in range(0, timeout_100ms):
         time.sleep(0.1)
-    thread_evt.set()
+    thread_evt.set()    # event set to True
+    _currtime = datetime.datetime.now(timezone('Asia/Seoul'))
+    print(str(_currtime) + ', [' + str(number) + '], thread event to \'set\'')
     _currtime = datetime.datetime.now(timezone('Asia/Seoul'))
     print(str(_currtime) + ', thread_test, number is \'' + str(number) + '\', time: \'' + str(float_time) + '\' [end]')
 
 def main():
     print('thread test program is started')
     _thread_evt_list    = []
-    _thread_unlocked      = False
+    _thread_unlocked    = False
 
     for _i in range(0,10):
         _thread_evt         = threading.Event()
         _time               = time.time()
 
         _thread_evt_list.append(_thread_evt)
-        _thread_evt.clear()
+        _thread_evt.clear() # event set to False
 
         _thread_test_var    = threading.Thread(target=thread_test, name="thread_test", args=(_i, _time, _thread_evt, ))
         _thread_test_var.start()
 
         time.sleep(0.05)
 
-    _wait_sec = 0.05
+    _wait_sec = 0.01
     while not _thread_unlocked:
         _thread_unlocked_and_acc   = True
-        for _i, _thread_evt in enumerate(_thread_evt_list):
-            _fr = _thread_evt.wait(_wait_sec)
-            _thread_unlocked_and_acc      &= _fr  # if one or more threads are released, it has True
+        for _i, _item_thread_evt in enumerate(_thread_evt_list):
+            _fr                         = _item_thread_evt.wait(_wait_sec)
+            _thread_unlocked_and_acc    &= _fr  # if one or more threads are released, it has True
             _currtime = datetime.datetime.now(timezone('Asia/Seoul'))
             if _fr:
                 print(str(_currtime) + ', wait result: ' + str(_fr) + ', ' + str(_i) + ' is released')
             else:
                 print(str(_currtime) + ', wait result: ' + str(_fr) + ', ' + str(_i) + ' is locked, wait: ' + str(_wait_sec) + 's')
-        _thread_unlocked  = _thread_unlocked_and_acc
-        if not _thread_unlocked:
-            print('_thread_unlocked: ' + str(_thread_unlocked))
 
-    for _i, _thread_evt in enumerate(_thread_evt_list):
-        _fr = _thread_evt.wait(0.05)
-        print('[' + str(_i) + '] Checking thread is unlocked: ' + str(_fr ))
+        _thread_unlocked = _thread_unlocked_and_acc
+        if _thread_unlocked_and_acc:
+            _thread_unlocked    = True
+            _currtime = datetime.datetime.now(timezone('Asia/Seoul'))
+            print(str(_currtime) + ', _thread_unlocked: ' + str(_thread_unlocked))
+
+    for _i, _item_thread_evt in enumerate(_thread_evt_list):
+        _fr = _item_thread_evt.wait(0.01)
+        _currtime = datetime.datetime.now(timezone('Asia/Seoul'))
+        if _fr:
+            print(str(_currtime) + ', [' + str(_i) + '] Checking thread is unlocked: ' + str(_fr ) + '(=event set())')
+        else:
+            print(str(_currtime) + ', [' + str(_i) + '] Checking thread is locked: ' + str(_fr ) + '(event clear())')
 
     while True:
         print('To exit, insert \'q\'')
