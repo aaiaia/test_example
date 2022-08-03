840cc71f917be5aa94417fe5969ed08890051b52
PleYa <aaiaia.gg@gmail.com>
2022-08-03 17:17:04 +0900

    fixed base.py to test current and caller function

--- python/debug/function_name/base.py
+++ python/debug/function_name/base.py
@@ -1,7 +1,14 @@
 import sys
+import imported_0, imported_1
+
+def function_in_base():
+    imported_1.example_function_1_0()
 
 def main():
-    pass
+    print('-> call from main() function')
+    imported_0.example_function_0_0()
+    print('-> call from function_in_base() function')
+    function_in_base()
 
 if __name__ == '__main__':
     main()
