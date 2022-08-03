cf2a1f45b6af1a55d00958b316078d01db19cfcf
PleYa <aaiaia.gg@gmail.com>
2022-08-03 17:23:11 +0900

    fixed print direction

--- python/debug/function_name/imported_0.py
+++ python/debug/function_name/imported_0.py
@@ -3,18 +3,20 @@ import get_code_info as ci
 
 def example_function_0_0():
     _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(0))
-    print('current [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    print('print direction:forward,  current [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
     _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(1))
-    print(' caller [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    print('print direction:forward,   caller [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    example_function_0_1()
 
 def example_function_0_1():
     _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(0))
-    print('current [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    print('print direction:forward,  current [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
     _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(1))
-    print(' caller [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    print('print direction:forward,   caller [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    example_function_0_2()
 
 def example_function_0_2():
     _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(0))
-    print('current [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    print('print direction:forward,  current [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
     _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(1))
-    print(' caller [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    print('print direction:forward,   caller [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
--- python/debug/function_name/imported_1.py
+++ python/debug/function_name/imported_1.py
@@ -2,19 +2,21 @@ import sys
 import get_code_info as ci
 
 def example_function_1_0():
+    example_function_1_1()
     _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(0))
-    print('current [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    print('print direction:backward, current [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
     _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(1))
-    print(' caller [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    print('print direction:backward,  caller [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
 
 def example_function_1_1():
+    example_function_1_2()
     _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(0))
-    print('current [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    print('print direction:backward, current [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
     _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(1))
-    print(' caller [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    print('print direction:backward,  caller [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
 
 def example_function_1_2():
     _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(0))
-    print('current [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    print('print direction:backward, current [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
     _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(1))
-    print(' caller [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    print('print direction:backward,  caller [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
