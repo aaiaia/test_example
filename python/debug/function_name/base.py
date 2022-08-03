2d35127ac9f78d289c487f97457d11fb05b4fda6
PleYa <aaiaia.gg@gmail.com>
2022-08-03 17:28:25 +0900

    fixed import name following filename

--- python/debug/function_name/base.py
+++ python/debug/function_name/base.py
@@ -1,12 +1,12 @@
 import sys
-import imported_0, imported_1
+import imported_code0 as lib0, imported_code1 as lib1
 
 def function_in_base():
-    imported_1.example_function_1_0()
+    lib1.example_function_1_0()
 
 def main():
     print('-> call from main() function')
-    imported_0.example_function_0_0()
+    lib0.example_function_0_0()
     print('-> call from function_in_base() function')
     function_in_base()
 
