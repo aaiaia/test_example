6b1c11cb3c8f21d0f6ee6a14a9d22f5739d57286
PleYa <aaiaia.gg@gmail.com>
2022-08-03 17:17:47 +0900

    fixed imported file print code info using get_code_info()

--- python/debug/function_name/imported_0.py
+++ python/debug/function_name/imported_0.py
@@ -1,8 +1,20 @@
+import sys
+import get_code_info as ci
+
 def example_function_0_0():
-    pass
+    _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(0))
+    print('current [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(1))
+    print(' caller [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
 
 def example_function_0_1():
-    pass
+    _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(0))
+    print('current [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(1))
+    print(' caller [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
 
 def example_function_0_2():
-    pass
+    _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(0))
+    print('current [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(1))
+    print(' caller [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
--- python/debug/function_name/imported_1.py
+++ python/debug/function_name/imported_1.py
@@ -1,8 +1,20 @@
+import sys
+import get_code_info as ci
+
 def example_function_1_0():
-    pass
+    _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(0))
+    print('current [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(1))
+    print(' caller [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
 
 def example_function_1_1():
-    pass
+    _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(0))
+    print('current [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(1))
+    print(' caller [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
 
 def example_function_1_2():
-    pass
+    _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(0))
+    print('current [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
+    _filename, _funcname, _codeline = ci.get_code_info(sys._getframe(1))
+    print(' caller [file:function:line] -> ' + _filename + ':' + _funcname + ':' + _codeline)
