6521d35984cf2a2087f0f161fb150076385923d2
PleYa <aaiaia.gg@gmail.com>
2022-08-03 14:37:05 +0900

    uploaded python source to get function name

---
+++ python/debug/function_name/imported_0.py
@@ -0,0 +1,8 @@
+def example_function_0_0():
+    pass
+
+def example_function_0_1():
+    pass
+
+def example_function_0_2():
+    pass
---
+++ python/debug/function_name/imported_1.py
@@ -0,0 +1,8 @@
+def example_function_1_0():
+    pass
+
+def example_function_1_1():
+    pass
+
+def example_function_1_2():
+    pass
