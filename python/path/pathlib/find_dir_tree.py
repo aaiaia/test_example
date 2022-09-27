912d755731bc6f5636295e23f3b9127cd99bccba
PleYa <aaiaia.gg@gmail.com>
2022-09-27 16:27:43 +0900

    added test function test()

--- python/path/pathlib/find_dir_tree.py
+++ python/path/pathlib/find_dir_tree.py
@@ -22,6 +22,20 @@ def search(path: str = './', name: str = '', ext: str = '', search_type: SEARCH_
     else:
         print('\'' + path + '\' is not exist')
 
+def test(path: str = './', name: str = '', ext: str = '', search_type: SEARCH_TYPE = SEARCH_TYPE.ALL):
+    _targetPath = Path(path)
+    if _targetPath.exists():
+        for _child in _targetPath.iterdir():
+            print('\'' + str(type(_child)) + '\', path: \'' + './' + str(_child) + '\'', end='')
+            if _child.is_dir():
+                print(' is_dir?: ' + str(_child.is_dir()))
+            elif _child.is_file():
+                print(' is_file?: ' + str(_child.is_file()))
+            else:
+                print('is unknown')
+    else:
+        print('\'' + path + '\' is not exist')
+
 def main(argv):
     print(argv)
     HELP_MSG = 'python3 [SOURCE_FILE] [PATH] [NAME]'
