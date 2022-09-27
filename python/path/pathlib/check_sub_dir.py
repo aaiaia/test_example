0e295205bade18c18d7ff726d66c548d60be1b09
PleYa <aaiaia.gg@gmail.com>
2022-09-27 15:19:26 +0900

    minar fixed typo

--- python/path/pathlib/check_sub_dir.py
+++ python/path/pathlib/check_sub_dir.py
@@ -12,9 +12,9 @@ def search(path: str = './', name: str = '', ext: str = '', search_type: SEARCH_
         for _child in _targetPath.iterdir():
             print('\'' + str(type(_child)) + '\', path: \'' + './' + str(_child) + '\'', end='')
             if _child.is_dir():
-                print('is_dir?: ' + str(_child.is_dir()))
+                print(' is_dir?: ' + str(_child.is_dir()))
             elif _child.is_file():
-                print('is_file?: ' + str(_child.is_file()))
+                print(' is_file?: ' + str(_child.is_file()))
             else:
                 print('is unknown')
     else:
@@ -22,16 +22,16 @@ def search(path: str = './', name: str = '', ext: str = '', search_type: SEARCH_
 
 def main():
     _testPath = './examples'
-    p = Path(_testPath )
+    p = Path(_testPath)
     if p.exists():
         for child in p.iterdir():
             print('\'' + str(type(child)) + '\', path: \'' + './' + str(child) + '\'', end='')
             if child.is_dir():
-                print('is_dir?: ' + str(child.is_dir()))
+                print(' is_dir?: ' + str(child.is_dir()))
             elif child.is_file():
-                print('is_file?: ' + str(child.is_file()))
+                print(' is_file?: ' + str(child.is_file()))
             else:
-                print('is unknown')
+                print(' is unknown')
     else:
         print('error!!! \'' + _testPath + '\' is not exist')
 
