29c37cc9c27a4a02a857f13d9b8d7afd8e300e2f
PleYa <aaiaia.gg@gmail.com>
2022-09-27 15:18:49 +0900

    added search function and SEARCH_TYPE class

--- python/path/pathlib/check_sub_dir.py
+++ python/path/pathlib/check_sub_dir.py
@@ -1,4 +1,24 @@
+from enum import Enum
 from pathlib import Path
+
+class SEARCH_TYPE(Enum):
+    ALL = 0
+    FILE = 1
+    DIR = 2
+
+def search(path: str = './', name: str = '', ext: str = '', search_type: SEARCH_TYPE = SEARCH_TYPE.ALL):
+    _targetPath = Path(path)
+    if _targetPath.exists():
+        for _child in _targetPath.iterdir():
+            print('\'' + str(type(_child)) + '\', path: \'' + './' + str(_child) + '\'', end='')
+            if _child.is_dir():
+                print('is_dir?: ' + str(_child.is_dir()))
+            elif _child.is_file():
+                print('is_file?: ' + str(_child.is_file()))
+            else:
+                print('is unknown')
+    else:
+        print('\'' + path + '\' is not exist')
 
 def main():
     _testPath = './examples'
@@ -14,6 +34,9 @@ def main():
                 print('is unknown')
     else:
         print('error!!! \'' + _testPath + '\' is not exist')
+
+    print('Call search function')
+    search()
 
 if __name__ == '__main__':
     main()
