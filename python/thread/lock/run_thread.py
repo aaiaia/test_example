c98ed7134018945d927567129d17e7142601817d
PleYa <aaiaia.gg@gmail.com>
2022-11-16 19:17:40 +0900

    fixed non blocking, blocking test

--- python/thread/lock/run_thread.py
+++ python/thread/lock/run_thread.py
@@ -9,21 +9,26 @@ __shared_var_non_block = 0
 __shared_var_block = 0
 
 def thread_acc_test(thread_number, float_time, acc_count:int, thread_lock=None, timeout_100ms=1):
-    global __shared_var_non_block
+    global __shared_var_non_block, __shared_var_block
     _currtime = datetime.datetime.now(timezone('Asia/Seoul'))
     print(str(_currtime) + ', thread_acc_test, thread_number is \'' + str(thread_number) + '\', time: \'' + str(float_time) + '\' [start]')
 
-    for _i in range(0, acc_count):
-        if thread_lock:
+    if thread_lock:
+        for _i in range(0, acc_count):
             thread_lock.acquire()
 
-        #there is test codes, thread access
-        __shared_var_non_block += 1
+            #there is test codes, thread access
+            __shared_var_block += 1
 
-        if thread_lock:
             thread_lock.release()
+    else:
+        for _i in range(0, acc_count):
+            #there is test codes, thread access
+            __shared_var_non_block += 1
 
-    print('__shared_var_non_block= ' + str(__shared_var_non_block))
+
+    print('__shared_var_non_block= ' + str(__shared_var_non_block) + ', __shared_var_block= ' + str(__shared_var_block))
+
     _currtime = datetime.datetime.now(timezone('Asia/Seoul'))
     print(str(_currtime) + ', thread_acc_test, thread_number is \'' + str(thread_number) + '\', time: \'' + str(float_time) + '\' [end]')
 
@@ -32,10 +37,17 @@ def main():
     _thread_is_done     = False
 
     _thread_lock        = threading.Lock()
+
     for _i in range(0,10):
         _time               = time.time()
 
-        _thread_acc_test_var    = threading.Thread(target=thread_acc_test, name="thread_acc_test", args=(_i, _time, 10000000, None, 5,))
+        _thread_acc_test_var    = threading.Thread(target=thread_acc_test, name="thread_acc_test_non_blocking", args=(_i, _time, 10000000, None, 5,))
+        _thread_acc_test_var.start()
+
+    for _i in range(10,20):
+        _time               = time.time()
+
+        _thread_acc_test_var    = threading.Thread(target=thread_acc_test, name="thread_acc_test_blocking", args=(_i, _time, 100000, _thread_lock, 5,))
         _thread_acc_test_var.start()
 
     while not _thread_is_done:
