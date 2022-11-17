03d12e0e9bd96fbe48f907650892cca99fcfe59b
PleYa <aaiaia.gg@gmail.com>
2022-11-17 11:59:48 +0900

    removed uesless arg. of thread_acc_test()

--- python/thread/lock/run_thread.py
+++ python/thread/lock/run_thread.py
@@ -8,7 +8,7 @@ from pytz import timezone
 __shared_var_non_block = 0
 __shared_var_block = 0
 
-def thread_acc_test(thread_number, acc_count:int, thread_lock=None, timeout_100ms=1):
+def thread_acc_test(thread_number, acc_count:int, thread_lock=None):
     global __shared_var_non_block, __shared_var_block
     _time = time.time()
     _currtime = datetime.datetime.now(timezone('Asia/Seoul'))
@@ -38,11 +38,11 @@ def main():
     _thread_lock        = threading.Lock()
 
     for _i in range(0,10):
-        _thread_acc_test_var    = threading.Thread(target=thread_acc_test, name="thread_acc_test_non_blocking", args=(_i, 100000, None, 5,))
+        _thread_acc_test_var    = threading.Thread(target=thread_acc_test, name="thread_acc_test_non_blocking", args=(_i, 100000, None,))
         _thread_acc_test_var.start()
 
     for _i in range(10,20):
-        _thread_acc_test_var    = threading.Thread(target=thread_acc_test, name="thread_acc_test_blocking", args=(_i, 100000, _thread_lock, 5,))
+        _thread_acc_test_var    = threading.Thread(target=thread_acc_test, name="thread_acc_test_blocking", args=(_i, 100000, _thread_lock,))
         _thread_acc_test_var.start()
 
     print('thread test program will be closed')
